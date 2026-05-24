2ceab63e8be00fbdfbd02bc9a31dd8cf7c819123
iwai.shogo <iwai.shogo@megachips.co.jp>
2026-05-24 14:01:48 +0900

    remoed unnecessary env variable

--- start_vllm_qwen3_14b_awq.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-export CUDA_HOME=/usr
-vllm serve Qwen/Qwen3-14B-AWQ \
-  --served-model-name local-model \
-  --enable-auto-tool-choice \
-  --tool-call-parser hermes \
-  --quantization awq_marlin \
-  --max-model-len 40960 \
-  --gpu-memory-utilization 0.94
